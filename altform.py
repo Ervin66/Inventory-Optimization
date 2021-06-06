1e9b1a4dc8b8aa04581891da301dcfdb17312679
Ervin Python <44205425+Ervin66@users.noreply.github.com>
2021-06-06 11:38:12 +0200

    Add files via upload

--- altform.py
+++ altform.py
@@ -7,6 +7,7 @@ import time
 # import io
 import json
 
+
 class InventoryModel():
     """class used to define and construct the inventory linear program
        data_path: the file path of the data containing the orders
@@ -29,20 +30,13 @@ class InventoryModel():
         self.raw_data[time_col] = pd.to_datetime(self.raw_data[time_col])
 
         # remove orders that do not have a location specified
-        self.raw_data = self.raw_data[self.raw_data[loc_col].notna()]
+        self.data = self.raw_data[self.raw_data[loc_col].notna()]
+
          # privremeno
         # v = self.raw_data[product_col].value_counts()
         # self.raw_data = self.raw_data[self.raw_data[product_col].isin(
         #     v.index[v.gt(100)])]
 
-        op_dic = {self.qty_col: "sum",
-                  "hold_cost_pallet": "first", 
-                  "lead_time": "first",
-                  "std_lead_time": "first",
-                  }
-        self.data = self.raw_data.groupby([self.raw_data[time_col].dt.isocalendar().week,
-                                           product_col,
-                                           loc_col], dropna=False).agg(op_dic)  # aggregate the order per week, product group and location
         print(self.data.groupby(product_col)[
               self.qty_col].agg(Mean='mean', Sum='sum', Size="size"))
         self.data.to_csv("demand_input.csv")
@@ -54,26 +48,23 @@ class InventoryModel():
         Required keywords argument:
         new_time_col: string of the aggregation basis for time (e.g. "week", "month",...)
         '''
-        self.prod_id = self.data.index.get_level_values(
-            self.product_col).unique().tolist()
-        self.time_id = self.data.index.get_level_values(  
-            new_time_col).unique().tolist()
-        print(len(self.time_id), "-----------------")
+        self.prod_id = self.data["sh_ItemId"].unique()
+        self.time_id = self.data["period"].unique()
         self.time_id = self.time_id[:self.maxt]
-        self.loc_id = self.loc_data.index
+        self.loc_id = self.data["new_shOrigin"].unique()
         self.factory_id = self.loc_data[self.loc_data["Echelon"]
                                         == "Factory"].index.tolist()
         self.dc_id = self.loc_data[self.loc_data["Echelon"]
                                    == "DC"].index.tolist()
         self.wh_id = self.loc_data[self.loc_data["Echelon"]
                                    == "Warehouse"].index.tolist()
+
         # self.loc_data = self.loc_data.loc[self.loc_id]
         # self.FTL_matrix = self.FTL_matrix.drop("CoP NL", axis=1) #temp
-        self.n_orders = len(self.data.loc[self.time_id])
         print(f"Number of product: {len(self.prod_id)}" + "\n",
               f"Number of locations: {len(self.loc_id)}" + "\n", 
               f"Number of periods: {len(self.time_id)}" + "\n",
-              f"Number of orders: {len(self.data.loc[self.time_id])}")
+              f"Number of orders: {len(self.data)}")
 
     def define_paramaters(self, loc_data_path, ftl_matrix):
         ''' Returns a dataframe of the paramaters required for the model
@@ -95,7 +86,7 @@ class InventoryModel():
             ["lead_time", "std_lead_time"]].first().fillna(0)
         self.lt_df["var_lead_time"] = self.lt_df["std_lead_time"] ** 2
         self.holding_costs = self.data.groupby(
-            ["sh_ItemId", "sh_OriginLocationMasterLocation"])["hold_cost_pallet"].first()
+            ["sh_ItemId", "new_shOrigin"])["hold_cost_pallet"].first()
 
     def define_variables(self):
         '''Defines the variable and their nature whcih are then added to the model
@@ -181,6 +172,7 @@ class InventoryModel():
         '''
         start_time = time.time()
 
+        factory_to_rw, cw_to_rw = self.routings_allocation()
         constr_dic = {}
         demand = self.data["Pallets"].to_dict()
         self.ss_df, ss = self.compute_ss(self.service_level)
@@ -232,20 +224,20 @@ class InventoryModel():
                                                                         for d in self.dc_id
                                                                         for t in self.time_id})
 
-        constr_dic.update({f"{w, t, i}3rdech_InvBal":LpAffineExpression((*((self.shipment[f][w][i][prevt[t]], 1) for f in self.factory_id),
-                                                                        *((self.shipment[d][w][i][prevt[t]], 1) for d in self.dc_id))) 
-                                                                        >= self.inv_level[i][w][t] + demand.get((t,i,w), 0)  - self.lost_sales[i][w][t]- self.inv_level[i][w][prevt[t]]*inital_inv[t]
+        constr_dic.update({f"{w, t, i}3rdech_InvBal":LpAffineExpression((*((self.shipment[f][w][i][prevt[t]], 1) for f in factory_to_rw[w]),
+                                                                        *((self.shipment[d][w][i][prevt[t]], 1) for d in cw_to_rw[w]))) 
+                                                                        >= self.inv_level[i][w][t] + demand.get((t,i,w), 0)  - self.lost_sales[i][w][t] - self.inv_level[i][w][prevt[t]]*inital_inv[t]
                                                                             for i in self.prod_id
                                                                             for w in self.wh_id
                                                                             for t in self.time_id})
 
         constr_dic.update({f"{d, t, i}2ndech_ssreq":LpAffineExpression([(self.inv_level[i][d][t], 1)]) 
-                                                                            >= ss["central_wh"].get(i, 0) 
+                                                                            >= ss["central_wh"].get(i, 0) - self.lost_sales[i][d][t]
                                                                             for i in self.prod_id
                                                                             for d in self.dc_id
                                                                             for t in self.time_id})
         constr_dic.update({f"{w, t, i}2ndech_ssreq":LpAffineExpression([(self.inv_level[i][w][t], 1)]) 
-                                                                        >= ss["regional_wh"].get(i, 0) 
+                                                                        >= ss["regional_wh"].get(i, 0) - self.lost_sales[i][w][t]
                                                                         for i in self.prod_id
                                                                         for w in self.wh_id
                                                                         for t in self.time_id})
@@ -269,24 +261,63 @@ class InventoryModel():
         return weight.values
 
     def compute_ss(self, service_level):
+        cent_wh_alloc = {} # store central warehouse - regional warehouse allocation
+        ss_cw = {} # central warehouse ss
+        cw_ss = pd.DataFrame() # store mean and std of demand aggregated per central warehouse and respective reg. warehouse
+        rw_ss = pd.DataFrame()
+        op = {self.qty_col:["mean", "std"],
+              "lead_time": "first",
+              "std_lead_time": "first"}
+        for c_wh in self.dc_id:
+            # extracting whih central warehosue is responsible for which regional warehouse
+            cent_wh_alloc[c_wh] = [c_wh] + self.loc_data.loc[self.loc_data["Resp. Central WH"] == c_wh].index.tolist()
+            # computing demand mean and std accroding to the allocation
+            stats = self.data[self.data["new_shOrigin"].isin(cent_wh_alloc[c_wh])].groupby("sh_ItemId", as_index=False).agg(op)
+            stats["Location"] = c_wh
+            cw_ss = cw_ss.append(stats)
+        cw_ss.set_index(["Location", "sh_ItemId"], inplace=True)
+        cw_ss.columns = ["Pallets_mean", "Pallets_std", "lead_time_mean", "lead_time_std"] # renaming the columns
+        cw_ss["Safety_Stock"] = self.service_level * (cw_ss["lead_time_mean"] * cw_ss["Pallets_std"]**2 
+                                      + cw_ss["Pallets_mean"]**2 * cw_ss["lead_time_std"]**2)**0.5
+        
+        for r_wh in self.wh_id:
+            stats = self.data[self.data["new_shOrigin"] == r_wh].groupby("sh_ItemId", as_index=False).agg(op)
+            stats["Location"] = r_wh
+            rw_ss = rw_ss.append(stats)
+        rw_ss.set_index(["Location", "sh_ItemId"], inplace=True)
+        rw_ss.columns = ["Pallets_mean", "Pallets_std", "lead_time_mean", "lead_time_std"] # renaming the columns
+        rw_ss["Safety_Stock"] = self.service_level * (rw_ss["lead_time_mean"] * rw_ss["Pallets_std"]**2)
+        ipdb.set_trace()
 
-        ss_cw = self.service_level * (self.lt_df["lead_time"] * self.demand_stats["var"] +
-                                      self.demand_stats["mean"]**2 * self.lt_df["var_lead_time"])**0.5
-        ss_rw = self.service_level * \
-            (self.lt_df["lead_time"] * self.demand_stats["var"])**0.5
-        ss_dic = {"central_wh": ss_cw, "regional_wh": ss_rw}
-        return pd.DataFrame(data=ss_dic, index=self.demand_stats.index), ss_dic
+        return cw_ss, rw_ss
+
+    def routings_allocation(self):
+        factory_to_rw = {}
+        cw_to_rw = {}
+        for w in self.wh_id:
+            factory_to_rw[w] = self.loc_data.loc[w, "Factory_allocation"].split(",")
+            
+            try:
+                cw_to_rw[w] = self.loc_data.loc[w, "Resp. Central WH"].split(",")
+            except AttributeError:
+                cw_to_rw[w]  = []
+        return factory_to_rw, cw_to_rw
 
     def build_model(self):
         ''' calls all the required function to initialize, solve and export the model
         '''
 
-        self.define_paramaters(loc_data_path="./CSV input files/wh_data.csv",
+        self.define_paramaters(loc_data_path="./CSV input files/wh_data_edited.csv",
                                ftl_matrix="./CSV input files/FTLmatrix.csv")
         self.define_indices(new_time_col="week")
+
+        start_time = time.time()
         self.define_variables()
         self.define_objective()
         self.define_constraints()
+        self.mc_time = time.time() - start_time
+
+        start_time = time.time()
         solver = CPLEX_PY()
         # self.inv_model.solve(solver)
         solver.buildSolverModel(self.inv_model)
@@ -294,8 +325,8 @@ class InventoryModel():
         solver.solverModel.parameters.workmem.set(6144)
         solver.callSolver(self.inv_model)
         status = solver.findSolutionValues(self.inv_model)
-        # in case the model is not solvable display which contraint(s) are conflicting:
-        solver.solverModel.parameters.conflict.display.set(2)
+        solver.solverModel.parameters.conflict.display.set(2)   # in case the model is not solvable display which contraint(s) are conflicting:
+        self.solv_time = time.time() - start_time
 
         # self.inv_model.writeLP("test.LP")
         # self.inv_model.writeMPS("test.mps")
@@ -325,12 +356,11 @@ class InventoryModel():
                             prod_ind=self.prod_id,
                             variable=self.production,
                             filename="Production")
-
-        for i in self.prod_id:
-            for t in self.time_id:
-                for l in self.loc_id:
-                    if self.lost_sales[i][l][t].varValue > 0:
-                        print((i,l,t), self.lost_sales[i][l][t].varValue)
+        self.export_vars_3d(time_ind=self.time_id,
+                            wh_ind=self.factory_id,
+                            prod_ind=self.prod_id,
+                            variable=self.lost_sales,
+                            filename="Lost Sales")
 
         # self.export_vars_3d(time_ind=self.time_id,
         #                     wh_ind=self.loc_id,
@@ -389,20 +419,25 @@ class InventoryModel():
 # Actually creating an instance of the classes in which the model is defined
 s = 1.95
 
-t=53
-time_dic = {}
+t = 53
+mctime_dic = {}
+solvtime_dic = {}
+
 start_time = time.time()
-I = InventoryModel(data_path="./CSV input files/orders_abc_xyz.csv",
+ 
+I = InventoryModel(data_path="./CSV input files/orders_newloc.csv",
                    product_col="sh_ItemId",
-                   time_col="sh_ShipmentDate",
-                   loc_col="sh_OriginLocationMasterLocation",
+                   time_col="period",
+                   loc_col="new_shOrigin",
                    qty_col="Pallets",
                    service_level=s,
                    maxt=t)
 I.build_model()
-time_taken = time.time() - start_time
-print("Total Time: ",time_taken)
 
+
+time_taken = time.time() - start_time
+print("Total Time: ", time_taken)
+   
 
 
 # calling the function that will assemble the model together
@@ -422,7 +457,6 @@ print("Total Time: ",time_taken)
 TODO:
 - automate time index
 - improve export functions (make indices argmuent list like)
-- generate default holding costs 
+- generate default holding costs
 - default value for holding costs
 '''
-
